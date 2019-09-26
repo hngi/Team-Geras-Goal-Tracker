f281ba57361444d5165f1b6d886809f634d04cc6
Gideon Markus <gideonmarkus@gmail.com>
2019-09-26 16:59:11 +0100

    Add files via upload
    Redesigned the signup and sign in pages

---
+++ login.php
@@ -0,0 +1,99 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Goal Tracker | Login</title>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
+<link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+
+<?php //Login.php
+require_once('dbcon.php');
+$error = $user = "";
+
+if (isset($_SESSION['user'])) {
+    echo '<meta http-equiv="refresh" content="0;url=dash.php">';
+    die();
+} else {
+    $user = "";
+}
+
+function sanitizeString($var)
+{
+    $var = strip_tags($var);
+    $var = htmlentities($var);
+    $var = stripslashes($var);
+    return $var;
+}
+
+function validEmail($mail) {
+    $mail = preg_match("^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$^",$mail);
+    
+    return $mail;
+}
+
+if (isset($_POST['email'])) {
+
+    $email = sanitizeString($_POST['email']);
+    $pass = sanitizeString($_POST['password']);
+    if ($email == "" || $pass == "") {
+        $error = "Not all fields were entered<br />";
+    } else {
+        
+        if (!validEmail($_POST['email'])) {
+            $error = "Invalid Email";
+        } else {
+            $salt1 = "qm&h*";
+            $salt2 = "pg!@";
+            $pass = md5("$salt1$pass$salt2");
+            $sql1 = "SELECT firstname, email, password FROM user WHERE email='$email' AND password='$pass'";
+            if (mysqli_num_rows(mysqli_query($conn,$sql1)) == 0) {
+                $error = "Email/Password invalid<br />";
+            } else {
+                
+                $_SESSION['user'] = $email;
+                echo '<meta http-equiv="refresh" content="0;url=dash.php">';
+                die();
+            }
+        
+        }
+
+    }
+}
+
+?>
+    
+<div class="container">
+    <div class="row">
+        <div class="col-md-6">
+            <div class="card">
+                <form class="box" method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" enctype="multipart/form-data">
+                    <h2>Login</h2>
+                    <p>You don't have an account? <a href="index.php">Sign up</a></p>
+                    <?php echo $error ?>
+                    <input type="email" class="email" name="email" placeholder="Email ">
+                    <input type="password" name="password" placeholder="Password"> <a class="forgot text-muted" href="#">Forgot password?</a> 
+                    <input type="submit" name="" value="Login">
+                    <div class="col-md-12">
+                        <ul class="social-network social-circle">
+                            <li><a href="#" class="icoFacebook" title="Facebook"><i class="fab fa-facebook-f"></i></a></li>
+                            <li><a href="#" class="icoTwitter" title="Twitter"><i class="fab fa-twitter"></i></a></li>
+                            <li><a href="#" class="icoGoogle" title="Google +"><i class="fab fa-google-plus"></i></a></li>
+                        </ul>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+    
+</body>
+</html>
